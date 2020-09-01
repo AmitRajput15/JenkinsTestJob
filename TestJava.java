dcea623f296a3f478582206b905cda60fcd243fc
AmitRajput15 <70574936+AmitRajput15@users.noreply.github.com>
2020-09-01 20:33:19 +0530

    Update TestJava.java

--- TestJava.java
+++ TestJava.java
@@ -2,7 +2,7 @@
 public class TestJava {
 
 	public static void main(String[] args) {
-		System.out.println("---------TestUpdated100-------");
+		System.out.println("---------TestUpdated500-------");
 
 	}
 
