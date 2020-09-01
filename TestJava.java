f85986785c092472b80a0088f7c37093b11467be
AmitRajput15 <70574936+AmitRajput15@users.noreply.github.com>
2020-09-01 20:41:51 +0530

    Update TestJava.java

--- TestJava.java
+++ TestJava.java
@@ -2,7 +2,7 @@
 public class TestJava {
 
 	public static void main(String[] args) {
-		System.out.println("---------Testng100 Webhook-------");
+		System.out.println("---------Testng500 Webhook-------");
 
 	}
 
