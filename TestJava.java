2538efc982da0b987bc871f10d08033b3f31ca6d
AmitRajput15 <70574936+AmitRajput15@users.noreply.github.com>
2020-09-01 20:34:37 +0530

    Update TestJava.java

--- TestJava.java
+++ TestJava.java
@@ -2,7 +2,7 @@
 public class TestJava {
 
 	public static void main(String[] args) {
-		System.out.println("---------TestUpdated500-------");
+		System.out.println("---------Testing Webhook-------");
 
 	}
 
